f921c47060dbf6f3fe133bfbbdd3976c2e5c6bd8
Marco Tzuc <mirzoz12@gmail.com>
2021-06-10 17:13:55 -0500

    hide db

--- deploy_scripts/db-config.sh
+++ deploy_scripts/db-config.sh
@@ -4,7 +4,7 @@
 sudo cd /var/www/globalwork/deploy
 
 # Remove if there is an existing database configuration
-sudo rm -f config/database.yml
+# sudo rm -f config/database.yml
 
 # Link the database configuration to shared db config file
-# sudo ln -s /var/www/globalwork/shared/config/database.yml config/database.yml
+sudo ln -s /var/www/globalwork/shared/config/database.yml config/database.yml
