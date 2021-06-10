2f5dd1348fad666b77740f67daf8b088cc71548e
Marco Tzuc <mirzoz12@gmail.com>
2021-06-10 16:36:55 -0500

    hide script

--- deploy_scripts/arrange-deployment-folders.sh
+++ deploy_scripts/arrange-deployment-folders.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Remove if previous deployment folder exists
-rm -rf /var/www/globalwork/prev-deploy
+# # Remove if previous deployment folder exists
+# rm -rf /var/www/globalwork/prev-deploy
 
-# Backup current deployment
-mv /var/www/globalwork/deploy /var/www/globalwork/prev-deploy
+# # Backup current deployment
+# mv /var/www/globalwork/deploy /var/www/globalwork/prev-deploy
 
 # Create new deploy folder and make nginx owner
 mkdir /var/www/globalwork/deploy
