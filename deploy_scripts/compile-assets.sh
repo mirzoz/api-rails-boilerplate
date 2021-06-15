b00ca8bdc78994f2a619beefba4bff4679f9ade6
Marco Tzuc <mirzoz12@gmail.com>
2021-06-15 15:49:47 -0500

    this is an api so we dont need assets

--- deploy_scripts/compile-assets.sh
+++ deploy_scripts/compile-assets.sh
@@ -8,4 +8,4 @@ sudo source /etc/profile
 
 # Run asset precompilation
 # - No need to run assets:clobber because this is a new folder. There should be none. We compile assets from the beginning in all deployments.
-RAILS_ENV=production bundle exec rake assets:precompile
+# RAILS_ENV=production bundle exec rake assets:precompile
