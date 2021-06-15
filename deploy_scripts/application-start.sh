331db55fc19c9415e4daab7f1bbd9d5b677c5bc2
Marco Tzuc <mirzoz12@gmail.com>
2021-06-15 11:32:39 -0500

    update files location

--- deploy_scripts/application-start.sh
+++ deploy_scripts/application-start.sh
@@ -5,10 +5,10 @@ sudo source /etc/profile
 
 # Print deployment info
 DEPLOYMENT_TIME=$( date -u "+%Y/%m/%d %H:%M:%S" )
-sudo echo "Deployment finished at: "$DEPLOYMENT_TIME" UTC" > /var/www/globalwork/deploy/public/deploy_time.txt
+sudo echo "Deployment finished at: "$DEPLOYMENT_TIME" UTC" > /home/ubuntu/apps/globalwork/deploy/public/deploy_time.txt
 
 # Arrange folder permissions
-sudo chown -R ubuntu:ubuntu /var/www/globalwork/deploy
+sudo chown -R ubuntu:ubuntu /home/ubuntu/apps/globalwork/deploy
 sudo chmod -R 775 /home/ubuntu/apps/globalwork/deploy
 
 sudo service nginx restart
